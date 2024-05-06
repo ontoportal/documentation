cf32a5cc23d17f4a6b99084cdb6a503ee5cefc49
Bilel KIHAL <kb_kihal@esi.dz>
2024-05-06 15:32:22 +0200

    hide portal logo from the navbar if the page is not existent for this portal

--- _includes/js/custom.js
+++ _includes/js/custom.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const currentUrl = window.location.href;
     document.querySelector(".nav-list").style.display = 'block'
-
     // check if we are in the user guid tabs
     if (currentUrl.includes('user_guide')) {
         const parts = currentUrl.split('/');
@@ -22,10 +21,17 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }
         
-        console.log('execute')
+        // Remove the logo of the portal from the nav if it doesn't have the current page
+        const nav_links = document.getElementById('ontoportal_tabs').querySelectorAll('a')
+        for(let i = 0; i<nav_links.length; i++){
+            fetch(nav_links[i].href)
+            .then(response => {
+                if (response.status === 404) {
+                    nav_links[i].remove()
+                }
+            });
+        }
     } else {
         document.querySelector(".breadcrumb-nav").style.display = 'block'
-        
     }
-
 });
